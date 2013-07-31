0e0188a46680a1c8515f71be93ddac0b7544d68b
Markwilliams <markwilliams@vm-markw-ubuntu.(none)>
2013-07-31 12:55:56 -0600

    User-specific environment variables file

---
+++ my_vars.rb
@@ -0,0 +1,7 @@
+$my_base_url       = "https://rally1.rallydev.com/slm"
+$my_username       = "user@company.com"
+$my_password       = "topsecret"
+$my_workspace      = "My Workspace"
+$my_project        = "My Project"
+$wsapi_version     = "1.43"
+$filename          = 'add_test_cases_to_testset.csv'
